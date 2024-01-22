a11760cac91ab49444c8ead7a5ade92add384b72
Natalia Zagórska <vermenea@gmail.com>
2024-01-22 18:32:28 +0100

    style: added some styling to nav and timer

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,27 @@
+import React from 'react';
 import webicon from '../../public/webicon.png';
 
-export default function NavBar() {
-	return (
-		<nav>
-			<ul>
-				<li>
-					<img src={webicon} alt='Web Icon' />
-					<h1 className='text-3xl'>PomoBuddy</h1>
-				</li>
-				<li>
-					<a href='#pomodoro'>pomodoro</a>
-				</li>
-				<li>
-					<a href='#'>tostudy</a>
-				</li>
-			</ul>
-		</nav>
-	);
-}
+const NavBar = () => {
+  return (
+    <nav className="navbar fixed w-full z-50">
+      <ul className="nav-list flex justify-center items-center flex-row font-oswald bg-white bg-opacity-0 rounded-lg shadow-md">
+        <li className="nav-item flex justify-center items-center list-none text-lg">
+          <img src={webicon} alt="Web Icon" className="webicon w-8" />
+          <h1 className="ml-2 font-bold">PomoBuddy</h1>
+        </li>
+        <li className="nav-item flex justify-center items-center list-none text-lg">
+          <a href="#pomodoro" className="nav-link">
+            Pomodoro
+          </a>
+        </li>
+        <li className="nav-item flex justify-center items-center list-none text-lg">
+          <a href="#" className="nav-link">
+            ToStudy
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
--- src/components/Pomodoro.jsx
+++ src/components/Pomodoro.jsx
@@ -1,46 +1,38 @@
+import React from 'react';
 
-export default function Pomodoro({title}) {
-	return (
-		<section>
-			<header>
-				<h1 id='pomodoro'>{title}</h1>
-			</header>
-			<p>
-				The Pomodoro Technique is a time management method developed by
-				Francesco Cirillo in the late 1980s. The technique is named after the
-				Italian word for "tomato" (pomodoro) because Cirillo initially used a
-				tomato-shaped kitchen timer to track his work intervals. The technique
-				is designed to improve focus, productivity, and time management by
-				breaking work into short, focused intervals, called "Pomodoros,"
-				typically lasting 25 minutes, followed by a short break of 5 minutes.
-			</p>
-			<ul>
-				<p>
-					The basic steps of the Pomodoro Technique include:
-				</p>
+const Pomodoro = ({ title }) => {
+  return (
+    <section className="pomo-section">
+      <header>
+        <h1 id="pomodoro" className="text-3xl font-bold">
+          {title}
+        </h1>
+      </header>
+      <p className="text text-xl mt-4">
+        The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s. The technique is named after the Italian word for "tomato" (pomodoro) because Cirillo initially used a tomato-shaped kitchen timer to track his work intervals. The technique is designed to improve focus, productivity, and time management by breaking work into short, focused intervals, called "Pomodoros," typically lasting 25 minutes, followed by a short break of 5 minutes.
+      </p>
+      <ul className="mt-4">
+        <p className="bold-text text-2xl">
+          The basic steps of the Pomodoro Technique include:
+        </p>
+        <li className="step text-xl">
+          Choose a task: Select a specific task or project that needs to be completed.
+        </li>
+        <li className="step text-xl">
+          Set the timer: Set a timer for 25 minutes, representing one Pomodoro. This time is dedicated solely to the chosen task.
+        </li>
+        <li className="step text-xl">
+          Work on the task: Focus completely on the task at hand until the timer rings. Avoid any distractions or interruptions during this period.
+        </li>
+        <li className="step text-xl">
+          Take a short break: After completing a Pomodoro, take a 5-minute break to relax and recharge. Use this time to stretch, grab a snack, or take a short walk.
+        </li>
+        <li className="step text-xl">
+          Repeat: Repeat the process with another Pomodoro. After completing four Pomodoros, take a longer break of 15-30 minutes.
+        </li>
+      </ul>
+    </section>
+  );
+};
 
-				<li>
-					Choose a task: Select a specific task or project that needs to be
-					completed.
-				</li>
-				<li>
-					Set the timer: Set a timer for 25 minutes, representing one Pomodoro.
-					This time is dedicated solely to the chosen task.
-				</li>
-				<li>
-					Work on the task: Focus completely on the task at hand until the timer
-					rings. Avoid any distractions or interruptions during this period.
-				</li>
-				<li>
-					Take a short break: After completing a Pomodoro, take a 5-minute break
-					to relax and recharge. Use this time to stretch, grab a snack, or take
-					a short walk.
-				</li>
-				<li>
-					Repeat: Repeat the process with another Pomodoro. After completing
-					four Pomodoros, take a longer break of 15-30 minutes.
-				</li>
-			</ul>
-		</section>
-	);
-}
+export default Pomodoro;
