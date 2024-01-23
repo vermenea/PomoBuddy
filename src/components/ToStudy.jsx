64daaddfe17ed1a67397ef17cd17f98f3afe2734
Natalia <mientuska00@gmail.com>
2024-01-23 19:10:24 +0100

    style: finished styling with tailwind

--- src/components/ToStudy.jsx
+++ src/components/ToStudy.jsx
@@ -1,12 +1,12 @@
 export default function ToStudy({ title }) {
 	return (
-		<section>
-			<header>
+		<section className='font-oswald m-6'>
+			<header className='text-3xl uppercase font-bold'>
 				<h1 id='tostudy'>{title}</h1>
 			</header>
-			<p>
-				To plan your study session add a new toStudy to the list.
-				Mark ✅ if it's done.
+			<p className='mt-4'>
+				To plan your study session add a new toStudy to the list. Mark ✅ if
+				it's done.
 			</p>
 			<h3>toDo list will be put in here soon ... 👀</h3>
 		</section>
