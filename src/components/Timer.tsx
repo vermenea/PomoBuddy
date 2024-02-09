b9858031f37237f6fb56f5eca36a65ae45fb4a9b
Natalia <mientuska00@gmail.com>
2024-02-09 20:07:34 +0100

    features: added breaks functionality

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -50,6 +50,16 @@ export default function Timer() {
     clearInterval(intervalIdRef.current as NodeJS.Timeout)
     intervalIdRef.current = null
   }
+  function handleReturnTime() {
+    setTime('25:00')
+  }
+
+  function handleShortBreak() {
+    setTime('05:00')
+  }
+  function handleLongBreak() {
+    setTime('15:00')
+  }
 
   return (
     <>
@@ -58,12 +68,29 @@ export default function Timer() {
         id="container"
       >
         <div className="flex flex-col items-center justify-evenly row-start-2 row-end-4 col-start-2 col-end-4 text-center bg-opacity-50 backdrop-blur-sm bg-white rounded-lg shadow-xl backdrop-filter backdrop-blur-6 border-1 border-opacity-30 w-64 md:w-full lg:w-full md:h-full lg:h-full max-h-96 p-10">
-          <div className="text-6xl md:text-8xl lg:text-8xl font-bold font-oswald text-red-600 p-5">
-            {time}
+          <div className="text-6xl md:text-8xl lg:text-8xl font-oswald text-red-600 p-5">
+            <div className="flex justify-between text-xs">
+              <button
+                onClick={handleShortBreak}
+                className="uppercase font-oswald text-white border-none bg-zinc-800 bg-opacity-60 transition-all hover:bg-opacity-100 shadow-lg rounded-md py-2 px-6 m-2"
+              >
+                short break
+              </button>
+              <button onClick={handleReturnTime} className="text-xl">
+                🔁
+              </button>
+              <button
+                onClick={handleLongBreak}
+                className="uppercase font-oswald text-white border-none bg-zinc-800 bg-opacity-60 transition-all hover:bg-opacity-100 shadow-lg rounded-md py-2 px-6 m-2"
+              >
+                long break
+              </button>
+            </div>
+            <div className="font-bold p-5">{time}</div>
           </div>
           <button
             onClick={isRunning ? handleStop : handleStart}
-            className="uppercase font-oswald text-white border-none bg-zinc-800 bg-opacity-90 transition-all hover:bg-opacity-100 shadow-lg rounded-md py-3 px-8"
+            className="uppercase font-oswald text-white border-none bg-zinc-800 bg-opacity-90 transition-all hover:bg-opacity-100 shadow-lg rounded-md py-2 px-6"
           >
             {isRunning ? 'stop' : 'start'}
           </button>
