e64d65b73d77b54299112ddcbf6a22c1c82bdffb
Natalia Zagórska <vermenea@gmail.com>
2024-02-26 21:42:09 +0100

    fix: fixed timer width

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,7 +1,7 @@
 import { useState, useRef } from 'react'
 import endSound from '../../public/ring.mp3'
 import startSound from '../../public/start.wav'
-import CreateToStudyButton from './BlackButton'
+import BlackButton from '../components/BlackButton'
 
 export default function Timer() {
   const [time, setTime] = useState<string>('25:00')
@@ -72,9 +72,26 @@ export default function Timer() {
         className="grid grid-rows-4 grid-cols-4 place-items-center bg-[background-img] bg-contain h-screen"
         id="container"
       >
-        <div className="flex flex-col items-center justify-evenly row-start-2 row-end-4 col-start-2 col-end-4 text-center bg-opacity-50 backdrop-blur-sm bg-white rounded-lg shadow-xl backdrop-filter backdrop-blur-6 border-1 border-opacity-30 w-64 md:w-full lg:w-full md:h-full lg:h-full max-h-96 p-10">
-          <div className="text-6xl md:text-8xl lg:text-8xl font-bold font-oswald text-red-600 p-5">
-            {time}
+        <div className="flex flex-col items-center justify-evenly row-start-2 row-end-4 col-start-2 col-end-4 text-center bg-opacity-50 backdrop-blur-sm bg-white rounded-lg shadow-xl backdrop-filter backdrop-blur-6 border-1 border-opacity-30 w-64 md:w-1/2 lg:w-1/2 md:h-full lg:h-full max-h-96 p-10">
+          <div className="text-6xl md:text-8xl lg:text-8xl font-oswald text-red-600 p-5">
+            <div className="flex justify-between text-xs">
+              <button
+                onClick={handleShortBreak}
+                className="uppercase font-oswald text-white border-none bg-zinc-800 bg-opacity-60 transition-all hover:bg-opacity-100 shadow-lg rounded-md py-2 px-6 m-2"
+              >
+                short break
+              </button>
+              <button onClick={handleReturnTime} className="text-xl">
+                🔁
+              </button>
+              <button
+                onClick={handleLongBreak}
+                className="uppercase font-oswald text-white border-none bg-zinc-800 bg-opacity-60 transition-all hover:bg-opacity-100 shadow-lg rounded-md py-2 px-6 m-2"
+              >
+                long break
+              </button>
+            </div>
+            <div className="font-bold p-5">{time}</div>
           </div>
           <button
             onClick={isRunning ? handleStop : handleStart}
@@ -83,7 +100,7 @@ export default function Timer() {
             {isRunning ? 'stop' : 'start'}
           </button>
         </div>
-        <CreateToStudyButton/>
+        <BlackButton />
       </div>
     </>
   )
