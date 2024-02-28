0562e0c726e1a4a3c756b74f289becb3d4f8c94c
Natalia Zagórska <vermenea@gmail.com>
2024-02-28 21:10:53 +0100

    refactor: cleaned up the code

--- src/components/BlackButton.tsx
+++ src/components/BlackButton.tsx
@@ -1,13 +1,6 @@
 export default function CreateToStudyButton() {
   return (
     <a
-      onClick={(e) => {
-        e.preventDefault()
-        const toStudyElement = document.getElementById('tostudy')
-        toStudyElement?.scrollIntoView({
-          behavior: 'smooth',
-        })
-      }}
       className="row-start-4 row-end-5 col-start-2 col-end-4 uppercase font-oswald text-white text-1.6rem rounded-12 p-4 bg-zinc-800 bg-opacity-80 transition-all hover:bg-opacity-100 shadow-lg rounded-md"
       href="#tostudy"
     >
