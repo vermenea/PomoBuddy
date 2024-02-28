1de6666b61d9f1674cfc1e0df04ef9b50da39827
Natalia Zagórska <vermenea@gmail.com>
2024-02-28 19:51:30 +0100

    build: fix bug with deployment

--- src/components/ToStudy.tsx
+++ src/components/ToStudy.tsx
@@ -5,7 +5,7 @@ interface ToStudyValue {
   pomodoros: number
 }
 
-export default function ToStudy() {
+const ToStudy: React.FC = () => {
   const [toStudyValue, setToStudyValue] = useState<string>('')
   const [estimatedPomodoros, setEstimatedPomodoros] = useState<number>(1)
   const [submittedToStudyValue, setSubmittedToStudyValue] = useState<
@@ -55,59 +55,60 @@ export default function ToStudy() {
       )
       return updatedToStudyValues
     })
-
-    return (
-      <section className="font-oswald max-w-4xl mx-auto p-10 md:px-16 lg:px-20 xl:px-20 scroll-smooth">
-        <header className="text-3xl uppercase font-bold">
-          <h1 id="tostudy">create a tostudy</h1>
-        </header>
-        <p className="mt-10 text-xl">
-          To plan your study session, add a new toStudy to the list, set
-          estimated pomodoros that you need to set for study, and mark ✅ if
-          it's done.
-        </p>
-
-        <div className="flex flex-col justify-start mt-10 border-4 w-full  rounded-md text-xl">
-          <input
-            type="text"
-            placeholder="Type here..."
-            className="w-inherit m-5  overflow-hidden"
-            value={toStudyValue}
-            onChange={handleInput}
-          ></input>
-          <div className="flex m-5">
-            <p>Estimated pomodoros:</p>
-            <input
-              type="number"
-              className="w-12 mr-2 text-right text-red-500"
-              value={estimatedPomodoros}
-              onChange={handleEstimatedPomodorosChange}
-            ></input>
-          </div>
-
-          <hr className="border-2"></hr>
-          <ul className="flex flex-col align-center m-8 ">
-            {submittedToStudyValue.map((toStudy, index) => (
-              <li className="flex items-center m-2" key={index}>
-                {toStudy.value} ➡️ {toStudy.pomodoros} pomodoros
-                <button
-                  type="button"
-                  className="ml-auto"
-                  onClick={() => handleCheckboxChange(index)}
-                >
-                  ✅
-                </button>
-              </li>
-            ))}
-          </ul>
-          <button
-            className="bg-red-500 text-white rounded-md py-2 px-2 w-fit ml-auto m-5"
-            onClick={handleSubmit}
-          >
-            Add toStudy
-          </button>
-        </div>
-      </section>
-    )
   }
+
+  return (
+    <section className="font-oswald max-w-4xl mx-auto p-10 md:px-16 lg:px-20 xl:px-20 scroll-smooth">
+      <header className="text-3xl uppercase font-bold">
+        <h1 id="tostudy">create a tostudy</h1>
+      </header>
+      <p className="mt-10 text-xl">
+        To plan your study session, add a new toStudy to the list, set estimated
+        pomodoros that you need to set for study, and mark ✅ if it's done.
+      </p>
+
+      <div className="flex flex-col justify-start mt-10 border-4 w-full  rounded-md text-xl">
+        <input
+          type="text"
+          placeholder="Type here..."
+          className="w-inherit m-5  overflow-hidden"
+          value={toStudyValue}
+          onChange={handleInput}
+        ></input>
+        <div className="flex m-5">
+          <p>Estimated pomodoros:</p>
+          <input
+            type="number"
+            className="w-12 mr-2 text-right text-red-500"
+            value={estimatedPomodoros}
+            onChange={handleEstimatedPomodorosChange}
+          ></input>
+        </div>
+
+        <hr className="border-2"></hr>
+        <ul className="flex flex-col align-center m-8 ">
+          {submittedToStudyValue.map((toStudy, index) => (
+            <li className="flex items-center m-2" key={index}>
+              {toStudy.value} ➡️ {toStudy.pomodoros} pomodoros
+              <button
+                type="button"
+                className="ml-auto"
+                onClick={() => handleCheckboxChange(index)}
+              >
+                ✅
+              </button>
+            </li>
+          ))}
+        </ul>
+        <button
+          className="bg-red-500 text-white rounded-md py-2 px-2 w-fit ml-auto m-5"
+          onClick={handleSubmit}
+        >
+          Add toStudy
+        </button>
+      </div>
+    </section>
+  )
 }
+
+export default ToStudy
