069e6215343e504d80c42d9eb3e5a630f676f738
Natalia Zagórska <vermenea@gmail.com>
2024-06-18 19:04:37 +0200

    fix: centered text on the button

--- app/components/main/CreateToStudyButton.tsx
+++ app/components/main/CreateToStudyButton.tsx
@@ -9,7 +9,7 @@ export default function CreateToStudyButton() {
 
 	return (
 		<motion.a
-			className='row-start-4 row-end-5 col-start-2 col-end-4 uppercase font-Oswald text-white text-1.6rem rounded-12 p-4 bg-zinc-800 bg-opacity-80 transition-all hover:bg-opacity-100 shadow-lg rounded-md'
+			className='row-start-4 row-end-5 col-start-2 col-end-4 uppercase font-Oswald text-white text-1.6rem text-center rounded-12 p-4 bg-zinc-800 bg-opacity-80 transition-all hover:bg-opacity-100 shadow-lg rounded-md'
 			href='#tostudy'
 			ref={ref}
 			style={displayAnimation}
