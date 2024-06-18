8558fc98bec486a339d8bf25d3ccc68c6224976b
Natalia Zagórska <vermenea@gmail.com>
2024-06-18 19:04:19 +0200

    feat: changed name displaying for more swag

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -2,12 +2,12 @@ export default function Footer() {
   return (
     <footer className="flex justify-center align-center bg-slate-200 font-Oswald p-4">
       <p>
-        coded by{' '}
+        coded by your scientist {}
         <a
           className="text-red-600 font-bold"
           href="https://github.com/vermenea"
         >
-          vermenea
+          Vermenea
         </a>
       </p>
     </footer>
