675626e6f4462430b81a7ef74e2fa14d9fbaa32d
Natalia Zagórska <vermenea@gmail.com>
2025-07-22 16:51:12 +0100

    feat: update setting font to self hosting

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,34 @@
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 import { getServerSession } from 'next-auth';
 import './style/globals.css';
-import { Inter } from 'next/font/google';
+import { VT323, Oswald } from 'next/font/google';
 import SessionProvider from './SessionProvider';
 import Login from './Login';
 import Home from './page';
 
-const inter = Inter({ subsets: ['latin'] });
+const vt323 = VT323({
+  weight: ['400'],
+  subsets: ['latin'],
+});
+
+const oswald = Oswald({
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
+});
 
 export default async function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	const session = await getServerSession(authOptions);
-	return (
-		<html lang='en'>
-			<body className={inter.className}>
-				<SessionProvider session={session}>
-					{!session ? <Login /> : <Home />}
-				</SessionProvider>
-			</body>
-		</html>
-	);
+  const session = await getServerSession(authOptions);
+  return (
+    <html lang='en'>
+      <body className={`${vt323.className} ${oswald.className}`}>
+        <SessionProvider session={session}>
+          {!session ? <Login /> : <Home />}
+        </SessionProvider>
+      </body>
+    </html>
+  );
 }
